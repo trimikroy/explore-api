8d14a973c50aa5ed2c99e634c1079123bca3c1a5
trimikpro <vupotiroy112@gmail.com>
2025-07-26 15:20:24 +0600

    api and json , json stringify and json perse

---
+++ json.js
@@ -0,0 +1,11 @@
+const user = { id: 1, name: "trim", job: "actor" };
+
+// JavaScript Object Notation -JSON
+const stringified = JSON.stringify(user);
+console.log(user);
+console.log(stringified);
+
+/* 
+    { id: 1, name: 'trim', job: 'actor' }
+    {"id":1,"name":"trim","job":"actor"}
+*/
